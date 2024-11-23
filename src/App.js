64f1a0f871806aa6b7e053149e74aad809eab386
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-23 14:22:42 +0900

    ✨ Feat : 라우팅 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/home/LandingPage";
+import NewsPage from "./pages/news/NewsPage";
 import QuizMain from "./pages/quiz/QuizMain";
 import QuizAnswer from "./pages/quiz/QuizAnswer";
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/news" element={<NewsPage />} />
       <Route path="/quiz" element={<QuizMain />} />
       <Route path="/quiz/:answer" element={<QuizAnswer />} />
     </Routes>
